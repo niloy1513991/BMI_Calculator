288045b1a803d21b56139c77b47e1904d097d06e
Nioy Kumar Mohonta <niloy@Nioys-MacBook-Pro.local>
2024-10-06 15:38:09 +0600

    Update App.jsx and other changes

--- src/App.jsx
+++ src/App.jsx
@@ -143,8 +143,33 @@ const App = () => {
             </h3>
             <p>{message}</p>
           </div>
-          <div className="mx-auto text-center text-slate-700">
-            &copy;niloykumarmohonta
+          <div className="copyright  text-slate-600 text-center h-14 w-72 mx-auto mt-2 flex flex-col">
+            <a href="mailto:niloykumarmohonta@gmail.com">
+              <span>&copy;niloykumarmohonta@gmail.com</span>
+            </a>
+            <span className="flex justify-around  h-8 w-28 mx-auto items-center">
+              <a
+                href="https://www.facebook.com/niloykumar.mohonta.5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="facebook.svg" alt="Facebook" className="h-6 w-6" />
+              </a>
+              <a
+                href="https://x.com/niloykmohonta"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="x.svg" alt="X" className="h-6 w-6" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/niloy-kumar-mohonta-bb3367155/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="linkedin.svg" alt="LinkedIn" className="h-8 w-auto" />
+              </a>
+            </span>
           </div>
         </form>
       </div>
